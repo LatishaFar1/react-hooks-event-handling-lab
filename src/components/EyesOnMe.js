12018c5081364d35977fb22553a50fece21e3e6d
Latisha Faroul <Lfaroul14@gmail.com>
2021-07-25 12:27:18 -0400

    keypad complete

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe(){
+    function eyesButton(event){
+        console.log(event);
+    }
+    return(
+    <button onBlur={console.log("Hey! Eyes on me!")} onFocus={console.log("Good!")}>Eyes On Me</button>
+
+    );
+    
+};
+
+
+
+
+export default EyesOnMe;
